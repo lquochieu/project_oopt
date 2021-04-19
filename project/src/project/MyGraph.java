db981ccf0a1c36c7c08564594153beb3ab4f9dff
JackAnh <64636218+JackAnh@users.noreply.github.com>
2021-04-19 09:08:17 +0700

    Add files via upload

--- project/src/project/MyGraph.java
+++ project/src/project/MyGraph.java
@@ -1,188 +1,157 @@
-package project;
-
-import org.graphstream.graph.*;
 import org.graphstream.graph.Graph;
 import org.graphstream.graph.implementations.SingleGraph;
+import scala.Int;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Scanner;
 
-import java.io.*;
-import java.util.*;
+public class MyGraph {
+    Graph graph = new SingleGraph("MyGraph");
+    HashMap<Integer, ArrayList<Integer>> allPath = new HashMap<>();    // Save all edge of the path
+    ArrayList<ArrayList<Integer>> adjacencyGraph = new ArrayList<ArrayList<Integer>>();  // Save the data of graph
+    ArrayList<Integer> currentPath = new ArrayList<>();
 
+    HashMap<String, Integer> count = new HashMap<>(); // Count the times of edge that went
+    int vertex = 0;
+    int edge = 0;
+    boolean[] flag;   // Đánh dấu DFS
 
-class MyGraph {
-    public Graph graph = new SingleGraph("NewGraph");
+    public MyGraph() { }
 
-    //hashmap co key la ten 1 dinh; value la cac dinh ke voi dinh key.
-    public HashMap<String,String[]> adjEdge=new HashMap<>();
-
-    //Dinh dich
-    String DesNode;
-
-    //luu duong di
-    public List<String> path=new ArrayList<>();
-
-    //danh sach cac dinh co it nhat 1 dinh ke
-    public List<String> hasNext=new ArrayList<>();
-
-    public void read() {
-        graph.setAutoCreate(true);
-        graph.setStrict(false);
-
-        //gia tri cua dinh dich
-        int max=0;
-
-        BufferedReader reader;
+    public void inputData() {
+        Scanner input = null;
+        File file = new File("D:\\Programming language\\Java\\Project Practice OOP\\MainGraphProject\\src\\main\\java\\data.txt");
         try {
-            //doc file text theo dong
-            reader = new BufferedReader(new FileReader(
-                    "output.txt"));
-            String line = reader.readLine();
-
-
-            //voi moi dong:
-            //add gia tri dau tien vao danh sach cac dinh co it nhat 1 dinh ke hasNext              (1)
-            //put vao adjmap (hashmap) ten dinh va danh sach cac dinh ke voi no.                    (2)
-            //them canh vao graph tu dong do                                                        (3)
-            //tim dinh dich                                                                         (4)
-            while (line != null) {
-                String s[] = line.split("\\s");
-                hasNext.add(s[0]);                                                                //(1)
-                adjEdge.put(s[0],s);                                                              //(2)
-
-                if(max< Integer.parseInt(s[0]))                                                   //(3)
-                    max=Integer.parseInt(s[0]);
-                for(int i=1;i<s.length;i++){
-                    if(max< Integer.parseInt(s[i]))                                               //(3)
-                        max=Integer.parseInt(s[i]);
-
-                    graph.addEdge(s[0]+s[i], s[0], s[i],true);                         //(4)
-
-                }
-                // read next line
-                line = reader.readLine();
-            }
-            reader.close();
-        } catch (IOException e) {
+            input = new Scanner(file);
+        } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
-        //gan gia tri dinh dich tim duoc.
-        DesNode=Integer.toString(max);
+        vertex = input.nextInt();  //Integer.parseInt(input.nextLine());
+        edge = input.nextInt();        //Integer.parseInt(input.nextLine());
 
-        //danh dau cac dinh co it nhat 1 dinh ke
-        for(Node node: graph){
-            node.setAttribute("ui.hasNext",0);
+        for (int i = 0; i <= vertex; ++i) { // 0 -> 8
+            adjacencyGraph.add(new ArrayList<Integer>());
         }
-        for(int j=0;j<hasNext.size();j++){
-            Node t=graph.getNode(hasNext.get(j));
-            t.setAttribute("ui.hasNext",1);
+
+        for (int i = 0; i < edge; ++i) {
+            int x = input.nextInt();  //Integer.parseInt(input.nextLine());
+            int y = input.nextInt();  //Integer.parseInt(input.nextLine());
+            String s = String.valueOf(x) + String.valueOf(y);
+            count.put(s,0);
+            adjacencyGraph.get(x).add(y);
         }
-    }
 
-
-    public void SetupGraph(){
-        //khoi tao thuoc tinh result
-        //0:canh khong thuoc duong di can tim
-        //1 canh thuong duong di can tim
-        graph.edges().forEach(e -> {
-            e.setAttribute("ui.result",0);
-        });
-
-        //khoi tao thuoc tinh: check ---- su dung trong dfs
-        //0:chua check ---- 1:da check.
-        for(Node node: graph){
-            node.setAttribute("ui.check",0);
+        // Khởi tạo cho DFS
+        flag = new boolean[vertex+1];
+        for (int i = 1; i <= vertex; ++i) flag[i] = false;
+        for (int i = 1; i <= vertex; ++i) {
+            ArrayList<Integer> arrayList = new ArrayList<>();
+            allPath.put(i, arrayList);
         }
     }
 
-    public void SetPath(){
-        //khoi tao duong di
-        path.add("1");
-        Node t=graph.getNode("1");
-        t.setAttribute("ui.check",1);
-    }
-
-    //dfs tu dinh p
-    public void dfs(String p){
-        //neu dinh p co it nhat 1 canh ke:
-        Node t=graph.getNode(p);
-        if(t.getAttribute("ui.hasNext").equals(1))
-        {
-            for (int i = 1; i < adjEdge.get(p).length; i++) {
-
-                //duyet toi dinh chua check;
-                String NextAdjVert = adjEdge.get(p)[i];
-                Node nav = graph.getNode(NextAdjVert);
-                if (nav.getAttribute("ui.check").equals(0)) {
-
-                    //them vao path(duong di) va danh dau la da check: "ui.check"
-                    path.add(NextAdjVert);
-                    nav.setAttribute("ui.check", 1);
-
-                    //Neu di toi dinh dich thi xu ly ket qua.
-                    if (NextAdjVert.equals(DesNode)) {
-                        for (int j = 0; j < path.size() - 1; j++) {
-                            Edge edge = graph.getEdge(path.get(j) + path.get(j + 1));
-                            System.out.print(path.get(j) + path.get(j + 1)+" ");
-                            edge.setAttribute("ui.result", 1);
+    public void DFS(int u) {
+        currentPath.add(u);
+        flag[u] = true;
+        for (int j = 0; j < adjacencyGraph.get(u).size(); ++j) {
+            int v = adjacencyGraph.get(u).get(j);
+            if (flag[v] == false) {
+                flag[v] = true;
+                DFS(v);
+                if (v == (vertex)) {
+                    for (int i = 0; i < currentPath.size() - 1; ++i) {
+                        ArrayList<Integer> arrayTemp = new ArrayList<>();
+                        arrayTemp = allPath.get(currentPath.get(i));
+                        if (!arrayTemp.contains(currentPath.get(i+1))) {
+                            arrayTemp.add(currentPath.get(i+1));
+                            allPath.put(currentPath.get(i), arrayTemp);
                         }
-                        System.out.println();
-
-                        //quay lui
-                        nav.setAttribute("ui.check", 0);
                     }
-
-                    //neu chua toi dich thi tiep tuc goi de quy thuat toan dfs
-                    else {
-                        dfs(NextAdjVert);
-
-                        //quay lui
-                        nav.setAttribute("ui.check", 0);
-                    }
-
-                    //quay lui
-                    path.remove(NextAdjVert);
                 }
+                currentPath.remove(currentPath.size()-1);
+                flag[v] = false;
             }
         }
     }
 
-    //hien thi graph
-    public void displayGraph(){
-        for (Node node : graph) {
-            node.setAttribute("ui.label", "       "+node.getId()+"     ");
+    public void drawGraph() {
+        System.setProperty("org.graphstream.ui", "swing");
+        for (int i = 1; i <= vertex; ++i) {
+            graph.addNode(String.valueOf(i));
+            graph.getNode(String.valueOf(i)).setAttribute("ui.style", "shape:circle; fill-color: yellow; size: 25px;");
+            graph.getNode(String.valueOf(i)).setAttribute("ui.label", String.valueOf(i));
         }
-        for (Node node : graph) {
-            node.getAttribute("ui.label");
+
+        for (int i = 1; i <= vertex; ++i) {
+            for (int j = 0; j < adjacencyGraph.get(i).size(); ++j) {
+                String s = String.valueOf(i) + String.valueOf(adjacencyGraph.get(i).get(j));
+                graph.addEdge(s, String.valueOf(i), String.valueOf(adjacencyGraph.get(i).get(j)), true);
+                graph.getEdge(s).setAttribute("ui.style", "size: 2px;");
+            }
         }
 
         graph.display();
     }
 
-    /*void printpath(){
-        graph.edges().forEach(e -> {
-           if(e.getAttribute("ui.result").equals(1)){
-                System.out.println(e.getId());}
-        });
-    }*/
-
-
-
-    public static void main(String args[]) {
-        System.setProperty("org.graphstream.ui", "swing");
-        MyGraph firstGraph = new MyGraph();
-
-        firstGraph.read();
-        firstGraph.SetupGraph();
-        firstGraph.SetPath();
-
-        firstGraph.dfs("1");
-       // firstGraph.printpath();
-        firstGraph.displayGraph();
-
+    public void simulation() {
+        Scanner input = new Scanner(System.in);
+        int u = 1; boolean flag = true;
+        graph.getNode(String.valueOf(u)).setAttribute("ui.style", "shape:circle;fill-color: green;size: 25px;");
+        while (flag) {
+            if(adjacencyGraph.get(u).size() == 0 || u == vertex) {
+                flag = false;
+                System.out.println("Congratulation");
+            } else {
+                System.out.println("Select vertex: ");
+                if (adjacencyGraph.get(u).size() == 0 && u != vertex) System.out.println("Not exist!!!");
+                else {
+                    for (int i = 0; i < adjacencyGraph.get(u).size(); ++i) {
+                        int v = adjacencyGraph.get(u).get(i);
+                        System.out.println("Select "+ v + ". " + u + " - " + v);
+                    }
+                    int choose = input.nextInt();
+                    String s = String.valueOf(u) + String.valueOf(choose);
+                    int tempCount = count.get(s);
+                    ++tempCount;
+                    count.put(s,tempCount);
+                    graph.getNode(String.valueOf(choose)).setAttribute("ui.style", "shape:circle;fill-color: green;size: 25px;");
+                    graph.getEdge(s).setAttribute("ui.style", "fill-color: rgb(102, 0, 255); size: 2px;");
+                    if (tempCount != 1) graph.getEdge(s).setAttribute("ui.label", tempCount);
+                    u = choose;
+                }
+            }
+        }
     }
 
+    public void simulationDFS() {
+        DFS(1);
+        Scanner input = new Scanner(System.in);
+        int u = 1;
+        int choose = 0;
+        while (choose != vertex) {
+            ArrayList<Integer> arrayTemp = new ArrayList<>();
+            arrayTemp = allPath.get(u);
+            for (int i = 0; i < arrayTemp.size(); ++i) {
+                System.out.println(arrayTemp.get(i) + "." + u + " - " + arrayTemp.get(i));
+            }
+            choose = input.nextInt();
+            String s = String.valueOf(u) + String.valueOf(choose);
+            graph.getNode(String.valueOf(choose)).setAttribute("ui.style", "shape:circle;fill-color: green;size: 25px;");
+            graph.getEdge(s).setAttribute("ui.style", "fill-color: rgb(102, 0, 255); size: 2px;");
+            if (choose == vertex) System.out.println("Congratulation");;
+            u = choose;
+        }
+    }
 
+    // Ham TEST
+    public static void main(String[] args) {
+        MyGraph myGraph = new MyGraph();
+        myGraph.inputData();
+        myGraph.drawGraph();
+        //myGraph.simulation();
+        myGraph.simulationDFS();
+    }
 }
